273fdc66a80b89aab7c523214e5396cb52cfc485
qiuyuting <qiuyuting@zjnu.edu.cn>
2025-03-19 22:45:12 +0800

    为 axios 实例添加请求拦截器和响应拦截器
    - 请求时：自动将本地存储中的 token 添加到 Authorization 请求头；
    - 处理响应时：检测到 401 错误，且非登录服务时，自动跳转到登录页面；

--- src/request.js
+++ src/request.js
@@ -5,4 +5,29 @@ const instance = axios.create({
   timeout: 2000,
 });
 
+
+instance.interceptors.request.use(function (config) {
+  const token = localStorage.getItem('token');
+
+  if (token) {
+    config.headers['Authorization'] = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
+
+instance.interceptors.response.use(
+  (response) => response,
+
+  // 超出 2xx 范围的状态码都会触发该函数。
+  function (error) {
+    if (error.status === 401 && error.config.url !== '/token') {
+      window.location.replace('/login/');
+    }
+
+    return Promise.reject(error);
+  }
+);
+
 export default instance;
