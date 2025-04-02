d230c3de7b1afe5738e905ffe00b920e85988357
qiuyuting <qiuyuting@zjnu.edu.cn>
2025-04-03 00:26:32 +0800

    照片预览时在右边显示照片详情以及照片中的人物

--- src/pages/index/Photo.vue
+++ src/pages/index/Photo.vue
@@ -20,24 +20,62 @@
 
   <v-divider class="my-4"></v-divider>
 
-  <div v-if="photo">
-    <v-img
-      :src="photo.src"
-      :lazy-src="photo.src"
-      :aspect-ratio="photo.metadata.width / photo.metadata.height"
-      contain
-    >
-      <!-- 图片加载失败时显示的默认图片 -->
-      <template v-slot:error>
-        <v-img height="200" width="200" src="/src/assets/image-placeholder.svg"></v-img>
-      </template>
-    </v-img>
+  <div v-if="photo" class="photo-container">
+    <div>
+      <v-img
+        :src="photo.src"
+        :lazy-src="photo.src"
+        :aspect-ratio="photo.metadata.width / photo.metadata.height"
+      >
+        <!-- 图片加载失败时显示的默认图片 -->
+        <template v-slot:error>
+          <v-img height="200" width="200" src="/src/assets/image-placeholder.svg"></v-img>
+        </template>
+      </v-img>
+    </div>
+    <div>
+      <v-list>
+        <v-list-subheader class="my-4" v-if="photo.faces?.length">人物</v-list-subheader>
+
+        <v-list-item
+          v-for="face in (photo.faces || [])/*.filter((face) => face.distance_from_who < 0.5)*/"
+          :key="face.who"
+          link
+          :to="`/person/${face.who}`"
+        >
+          <v-avatar size="72" class="border border-white" color="primary">
+            <v-img :src="face.face_base64 ? `data:image/jpeg;base64, ${face.face_base64}` : '/src/assets/image-placeholder.svg'" ></v-img>
+          </v-avatar>
+        </v-list-item>
+
+        <v-list-subheader class="my-4">详情</v-list-subheader>
+
+        <!-- 上传日期 -->
+        <v-list-item>
+          <template #prepend>
+            <v-icon color="primary">mdi-calendar</v-icon>
+          </template>
+          <v-list-item-title>{{photo.dateStr}}</v-list-item-title>
+          <v-list-item-subtitle>{{photo.timeStr}}</v-list-item-subtitle>
+        </v-list-item>
+
+        <!-- 照片信息 -->
+        <v-list-item class="mt-8">
+          <template #prepend>
+            <v-icon color="primary">mdi-image</v-icon>
+          </template>
+          <v-list-item-title style="white-space: normal">{{photo.original_filename}}</v-list-item-title>
+          <v-list-item-subtitle>{{photo.mp}} {{photo.metadata.width}} × {{photo.metadata.height}}</v-list-item-subtitle>
+        </v-list-item>
+      </v-list>
+    </div>
   </div>
 
 </template>
 
 <script setup>
 import { ref } from 'vue';
+import dayjs from 'dayjs';
 import { useAsyncState } from '@vueuse/core'
 import { useRoute, useRouter } from 'vue-router';
 import request from '/src/request.js';
@@ -58,7 +96,16 @@ const {
       url: `/photos/${route.params.id}`,
     });
 
-    return response.data.data;
+    const photo = response.data.data;
+
+    const createdAt = dayjs(photo.created_at);
+
+    return {
+      ...photo,
+      dateStr: createdAt.format('YYYY年MM月DD日'),
+      timeStr: createdAt.format('ddd HH:mm'),
+      mp: `${(photo.metadata.width * photo.metadata.height / 1e6).toFixed(1)} MP`
+    };
   } catch (e) {
     if (e.name === 'AxiosError') {
       throw new Error(e.response.data?.msg || '服务异常！');
@@ -71,5 +118,9 @@ const {
 </script>
 
 <style scoped>
-
+.photo-container {
+  display: grid;
+  grid-template-columns: 1fr 400px;
+  grid-gap: 32px;
+}
 </style>
